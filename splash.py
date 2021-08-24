fbbb54ee3d495e9d7501f84d63e7f4f452af2084
Liton Nath <59998453+litonnath@users.noreply.github.com>
2021-08-25 00:49:25 +0530

    Update splash.py

--- splash.py
+++ splash.py
@@ -10,17 +10,20 @@ class splash(scrapy.Spider):
     start_urls=[
         'https://search.ipindia.gov.in/DesignApplicationStatus/'
     ]
-
+    
+    #Open Url with Splash 
     def parse1(self,response):
 
         return SplashRequest(url='https://search.ipindia.gov.in/DesignApplicationStatus/',callback=self.parse2,endpoint='render.html', args={'wait': 0.5})
+    
+    #Set Break point with post form data
 
     def parse2(self,response):
         pdb.set_trace()
 
         yield scrapy.FormRequest("https://search.ipindia.gov.in/DesignApplicationStatus/", formdata={'APPLICATION_NUMBER': 222230},  callback=self.parse,meta={ 'splash': { 'args': {  'html': 1, 'png': 1 }} }, dont_filter=True)
 
-   #After login ,Data Fetech below script
+   #After login ,Data Fetech below script 
     def parse(self, response):
         open_in_browser(response)
         yield {
